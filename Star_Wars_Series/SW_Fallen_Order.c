50598a3fa77218493d04a48550daeaba8c76b467
Michael-MSL <michael.schneider_uh@proton.me>
2024-05-06 23:59:50 -0500

    Update SW_Fallen_Order.c

--- Star_Wars_Series/SW_Fallen_Order.c
+++ Star_Wars_Series/SW_Fallen_Order.c
@@ -7,3 +7,15 @@ Engine.CheatManager:BugItGo.Pitch
 Engine.CheatManager:BugItGo.Z
 Engine.CheatManager:BugItGo.Y
 Engine.CheatManager:BugItGo.X
+[ Index:000007D0D] (Size:0x000B8) [UObject:000000000A9DD0C0] Function  /Script/Engine.CheatManager:BugItStringCreator
+[ Index:000007D0E] (Size:0x00070) [UObject:000000000A9E5870] StrProperty  /Script/Engine.CheatManager:BugItStringCreator.LocString
+[ Index:000007D0F] (Size:0x00070) [UObject:000000000A9E5800] StrProperty  /Script/Engine.CheatManager:BugItStringCreator.GoString
+[ Index:000007D10] (Size:0x00078) [UObject:000000000AA13180] StructProperty  /Script/Engine.CheatManager:BugItStringCreator.ViewRotation	~	ScriptStruct  /Script/CoreUObject.Rotator
+[ Index:000007D11] (Size:0x00078) [UObject:000000000AA13100] StructProperty  /Script/Engine.CheatManager:BugItStringCreator.ViewLocation	~	ScriptStruct  /Script/CoreUObject.Vector
+[ Index:000007D12] (Size:0x000B8) [UObject:000000000A9DD000] Function  /Script/Engine.CheatManager:ChangeSize
+[ Index:000007D13] (Size:0x00070) [UObject:000000000A9E5790] FloatProperty  /Script/Engine.CheatManager:ChangeSize.F
+[ Index:000007D14] (Size:0x000B8) [UObject:000000000A9DCF40] Function  /Script/Engine.CheatManager:CheatScript
+[ Index:000007D15] (Size:0x00070) [UObject:000000000A9E5720] StrProperty  /Script/Engine.CheatManager:CheatScript.ScriptName
+[ Index:000007D16] (Size:0x000B8) [UObject:000000000A9DCE80] Function  /Script/Engine.CheatManager:ClearLevelStreamingStatus
+[ Index:000007D17] (Size:0x00070) [UObject:000000000A9E56B0] NameProperty  /Script/Engine.CheatManager:ClearLevelStreamingStatus.PackageName
+[ Index:000007D18] (Size:0x000B8) [UObject:000000000A9DCDC0] Function  /Script/Engine.CheatManager:DamageTarget
