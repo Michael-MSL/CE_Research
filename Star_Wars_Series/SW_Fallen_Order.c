35065383ecedc59a7b87d55d513cb74931b45dd7
Michael-MSL <michael.schneider_uh@proton.me>
2024-05-07 00:03:06 -0500

    Update SW_Fallen_Order.c

--- Star_Wars_Series/SW_Fallen_Order.c
+++ Star_Wars_Series/SW_Fallen_Order.c
@@ -30,3 +30,14 @@ Engine.CheatManager:DebugCapsuleSweepComplex.bTraceComplex
 Engine.CheatManager:DebugCapsuleSweepPawn
 Engine.CheatManager:DebugCapsuleSweepSize
 Engine.CheatManager:DebugCapsuleSweepSize.Radius
+[ Index:000007D24] (Size:0x00070) [UObject:000000000A9E7240] FloatProperty  /Script/Engine.CheatManager:DebugCapsuleSweepSize.HalfHeight
+[ Index:000007D25] (Size:0x000B8) [UObject:000000000A9DC7C0] Function  /Script/Engine.CheatManager:DestroyAll
+[ Index:000007D26] (Size:0x00080) [UObject:000000000AA12F80] ClassProperty  /Script/Engine.CheatManager:DestroyAll.aClass	~	Class  /Script/CoreUObject.Class
+[ Index:000007D27] (Size:0x000B8) [UObject:000000000A9DC700] Function  /Script/Engine.CheatManager:DestroyAllPawnsExceptTarget
+[ Index:000007D28] (Size:0x000B8) [UObject:000000000A9DC640] Function  /Script/Engine.CheatManager:DestroyPawns
+[ Index:000007D29] (Size:0x00080) [UObject:000000000AA12F00] ClassProperty  /Script/Engine.CheatManager:DestroyPawns.aClass	~	Class  /Script/CoreUObject.Class
+[ Index:000007D2A] (Size:0x000B8) [UObject:000000000A9DF700] Function  /Script/Engine.CheatManager:DestroyServerStatReplicator
+[ Index:000007D2B] (Size:0x000B8) [UObject:000000000A9DF640] Function  /Script/Engine.CheatManager:DestroyTarget
+[ Index:000007D2C] (Size:0x000B8) [UObject:000000000A9DF580] Function  /Script/Engine.CheatManager:DisableDebugCamera
+[ Index:000007D2D] (Size:0x000B8) [UObject:000000000A9DF4C0] Function  /Script/Engine.CheatManager:DumpChatState
+[ Index:000007D2E] (Size:0x000B8) [UObject:000000000A9DF400] Function  /Script/Engine.CheatManager:DumpLevelMemoryInfo
