8c94cabc53131703b4501387f9c1ace76d809af5
Michael-MSL <michael.schneider_uh@proton.me>
2024-05-07 00:01:18 -0500

    Update SW_Fallen_Order.c

--- Star_Wars_Series/SW_Fallen_Order.c
+++ Star_Wars_Series/SW_Fallen_Order.c
@@ -15,7 +15,7 @@ Engine.CheatManager:BugItStringCreator.ViewLocation	~	ScriptStruct  /Script/Core
 Engine.CheatManager:ChangeSize
 Engine.CheatManager:ChangeSize.F
 Engine.CheatManager:CheatScript
-[ Index:000007D15] (Size:0x00070) [UObject:000000000A9E5720] StrProperty  /Script/Engine.CheatManager:CheatScript.ScriptName
-[ Index:000007D16] (Size:0x000B8) [UObject:000000000A9DCE80] Function  /Script/Engine.CheatManager:ClearLevelStreamingStatus
-[ Index:000007D17] (Size:0x00070) [UObject:000000000A9E56B0] NameProperty  /Script/Engine.CheatManager:ClearLevelStreamingStatus.PackageName
-[ Index:000007D18] (Size:0x000B8) [UObject:000000000A9DCDC0] Function  /Script/Engine.CheatManager:DamageTarget
+Engine.CheatManager:CheatScript.ScriptName
+Engine.CheatManager:ClearLevelStreamingStatus
+Engine.CheatManager:ClearLevelStreamingStatus.PackageName
+Engine.CheatManager:DamageTarget
