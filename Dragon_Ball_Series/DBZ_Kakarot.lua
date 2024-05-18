055449eae22e98d468cf6b7c3f93360260fa3eb9
Michael <michael.schneider_uh@proton.me>
2024-05-18 10:50:11 -0500

    Update DBZ_Kakarot.lua

--- Dragon_Ball_Series/DBZ_Kakarot.lua
+++ Dragon_Ball_Series/DBZ_Kakarot.lua
@@ -3,3 +3,9 @@ Engine.CheatManager:BugIt.ScreenShotDescription
 Engine.CheatManager:BugItGo
 Engine.CheatManager:BugItGo.Roll
 Engine.CheatManager:BugItGo.Yaw
+Engine.CheatManager:BugItGo.Pitch
+Engine.CheatManager:BugItGo.Z
+Engine.CheatManager:BugItGo.Y
+Engine.CheatManager:BugItGo.X
+Engine.CheatManager:BugItStringCreator
+Engine.CheatManager:BugItStringCreator.LocString
